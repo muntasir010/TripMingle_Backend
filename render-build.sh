0a3334eb8a7104ae81587f0b8e48bacf8166dd10
muntasir010 <naeemtasir03@gmail.com>
2026-01-03 14:44:39 +0600

    update render.sh

--- render-build.sh
+++ render-build.sh
@@ -6,3 +6,5 @@ set -o errexit
 npm install
 npx prisma generate
 npm run build
+npm install --include=dev && npx prisma generate && npm run build
+npm install --save-dev @types/express @types/cors @types/cookie-parser @types/jsonwebtoken @types/multer @types/node @types/nodemailer
