626da390af31bcb4312486301f43696f9a955baf
muntasir010 <naeemtasir03@gmail.com>
2026-01-03 12:24:34 +0600

    render deploy

---
+++ prisma/migrations/20260103062134_add_premium_until/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "premiumUntil" TIMESTAMP(3);
---
+++ prisma/migrations/20260103062355_add_verify_badge/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "verifiedBadge" BOOLEAN NOT NULL DEFAULT false;
