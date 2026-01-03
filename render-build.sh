569e550dbf8a73b4e551b86b5336fb135480c24d
muntasir010 <naeemtasir03@gmail.com>
2026-01-03 12:44:40 +0600

    added cors

--- render-build.sh
+++ render-build.sh
@@ -4,6 +4,5 @@
 set -o errexit
 
 npm install
-npm run build
 npx prisma generate
-npx prisma migrate deploy
+npm run build
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,34 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
-import router from './app/routes';
-import notFound from './app/middleware/notFound';
-import globalErrorHandler from './app/middleware/globalErrorHandler';
-import cookieParser from 'cookie-parser';
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import router from "./app/routes";
+import notFound from "./app/middleware/notFound";
+import globalErrorHandler from "./app/middleware/globalErrorHandler";
+import cookieParser from "cookie-parser";
+import config from "./config";
 
 const app: Application = express();
-app.use(cors({
-    origin: 'http://localhost:5000',
-    credentials: true
-}));
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000", config.frontend_url],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1', router);
+app.use("/api/v1", router);
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        Message: "Server is running..."
-    })
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "Server is running...",
+  });
 });
 
-app.use(notFound)
+app.use(notFound);
 
 app.use(globalErrorHandler);
-
-
 
 export default app;
